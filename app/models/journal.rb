03176d0b1e53b0ff40387c0c1fb6ae9ab3eca8c0
Kevin Liou <kwliou@hotmail.com>
2011-05-03 20:21:35 -0700

    edited journal json

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,3 +1,9 @@
 class Journal < ActiveRecord::Base
   belongs_to :plant
+  
+  def as_json(options={})
+    revised = super(options)
+    revised["created_at"] = (created_at.to_f * 1000).to_i # return created_at as milliseconds
+    return revised
+  end
 end
