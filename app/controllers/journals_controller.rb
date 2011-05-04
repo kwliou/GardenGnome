6aee91b7541096294b2d2ef4f1633065277ec37a
Kevin Liou <kwliou@hotmail.com>
2011-05-03 19:21:54 -0700

    edited journals

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -43,6 +43,7 @@ class JournalsController < ApplicationController
   # POST /journals.xml
   def create
     @plant = Plant.find(params[:plant_id])
+    params[:journal][:created_at] = Time.at(params[:journal][:created_at]) if params[:journal].has_key(:created_at)
     @journal = @plant.journals.build(params[:journal])
 
     respond_to do |format|
